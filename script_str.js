c1d1de14f5baa4cf6786536acea5dd6b548ce534
Daan van Rasmhorst <daanvr@gmail.com>
2018-11-16 13:44:18 +0100

    crousel

--- script_str.js
+++ script_str.js
@@ -3,26 +3,39 @@ $(".NavBtnHome").click(function () {
     return false;
 });
 
-count = 0;
-for (var i = 0; i <= 10; i++) {
-    $.ajax({
-        url: 'https://daanvr.github.io/de_straat_op_maat/img/Str1/imgs/' + i + '.jpg',
-        type: 'HEAD',
-        error: function () {
-            alert('EMPTY FOLDER');
-        },
-        success: function () {
-            count++;
-            $('#target').append('<div id="img_div_' + i + '"><img src=" https://daanvr.github.io/de_straat_op_maat/img/Str1/imgs/' + i + '.jpg"><div>');
-        }
-    });
-}
+// count = 0;
+// for (var i = 0; i <= 10; i++) {
+//     $.ajax({
+//         url: 'https://daanvr.github.io/de_straat_op_maat/img/Str1/imgs/' + i + '.jpg',
+//         type: 'HEAD',
+//         error: function () {
+//             console.log(i + ".jpg not found");
+//             // alert('EMPTY FOLDER');
+//         },
+//         success: function () {
+//             count++;
+//             $('.carousel').append('<div id="img_div_' + i + '"><img src=" https://daanvr.github.io/de_straat_op_maat/img/Str1/imgs/' + i + '.jpg"><div>');
+//         }
+//     });
+// }
+
+
+$('.carousel').append('<div class="Carouimgdiv" id="img_div_1"><img class="Carouimg" src="https://daanvr.github.io/de_straat_op_maat/img/Str1/imgs/1.jpg"><div>');
+$('.carousel').append('<div class="Carouimgdiv" id="img_div_2"><img class="Carouimg" src="https://daanvr.github.io/de_straat_op_maat/img/Str1/imgs/2.JPG"><div>');
+$('.carousel').append('<div class="Carouimgdiv" id="img_div_3"><img class="Carouimg" src="https://daanvr.github.io/de_straat_op_maat/img/Str1/imgs/3.JPG"><div>');
+$('.carousel').append('<div class="Carouimgdiv" id="img_div_4"><img class="Carouimg" src="https://daanvr.github.io/de_straat_op_maat/img/Str1/imgs/4.JPG"><div>');
+$('.carousel').append('<div class="Carouimgdiv" id="img_div_5"><img class="Carouimg" src="https://daanvr.github.io/de_straat_op_maat/img/Str1/imgs/5.JPG"><div>');
+
+
 
 $(document).ready(function () {
     $('.carousel').slick({
         slidesToShow: 3,
+        infinite: true,
         dots: true,
+        variableWidth: true,
         slidesToScroll: 1
+        // centerMode: true,
 
         //   setting-name: setting-value
     });
