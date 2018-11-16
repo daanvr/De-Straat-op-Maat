4778c503f3a276ed5758e40fc749d6cf5c7fce59
Daan van Rasmhorst <daanvr@gmail.com>
2018-11-16 13:07:14 +0100

    img carousel

--- script_str.js
+++ script_str.js
@@ -1,4 +1,29 @@
-$(".NavBtnHome").click(function() {
+$(".NavBtnHome").click(function () {
     window.location = "https://daanvr.github.io/de_straat_op_maat/index.html";
     return false;
+});
+
+count = 0;
+for (var i = 0; i <= 10; i++) {
+    $.ajax({
+        url: 'https://daanvr.github.io/de_straat_op_maat/img/Str1/imgs/' + i + '.jpg',
+        type: 'HEAD',
+        error: function () {
+            alert('EMPTY FOLDER');
+        },
+        success: function () {
+            count++;
+            $('#target').append('<div id="img_div_' + i + '"><img src=" https://daanvr.github.io/de_straat_op_maat/img/Str1/imgs/' + i + '.jpg"><div>');
+        }
+    });
+}
+
+$(document).ready(function () {
+    $('.carousel').slick({
+        slidesToShow: 3,
+        dots: true,
+        slidesToScroll: 1
+
+        //   setting-name: setting-value
+    });
 });
