2568903f569419d0b3293b704a51911328435631
Daan van Rasmhorst <daanvr@gmail.com>
2018-11-16 14:52:52 +0100

    oud en nieuw in progress

--- script_str.js
+++ script_str.js
@@ -21,22 +21,41 @@ $(".NavBtnHome").click(function () {
 
 
 $('.carousel').append('<div class="Carouimgdiv" id="img_div_1"><img class="Carouimg" src="https://daanvr.github.io/de_straat_op_maat/img/Str1/imgs/1.jpg"><div>');
-$('.carousel').append('<div class="Carouimgdiv" id="img_div_2"><img class="Carouimg" src="https://daanvr.github.io/de_straat_op_maat/img/Str1/imgs/2.JPG"><div>');
-$('.carousel').append('<div class="Carouimgdiv" id="img_div_3"><img class="Carouimg" src="https://daanvr.github.io/de_straat_op_maat/img/Str1/imgs/3.JPG"><div>');
-$('.carousel').append('<div class="Carouimgdiv" id="img_div_4"><img class="Carouimg" src="https://daanvr.github.io/de_straat_op_maat/img/Str1/imgs/4.JPG"><div>');
-$('.carousel').append('<div class="Carouimgdiv" id="img_div_5"><img class="Carouimg" src="https://daanvr.github.io/de_straat_op_maat/img/Str1/imgs/5.JPG"><div>');
+$('.carousel').append('<div class="Carouimgdiv" id="img_div_2"><img class="Carouimg" src="https://daanvr.github.io/de_straat_op_maat/img/Str1/imgs/2.JPG">Autoparkeren aan rechterzijde en overige voorzieningen aan linkerzijde<div>');
+// $('.carousel').append('<div class="Carouimgdiv" id="img_div_3"><img class="Carouimg" src="https://daanvr.github.io/de_straat_op_maat/img/Str1/imgs/3.JPG"><div>');
+$('.carousel').append('<div class="Carouimgdiv" id="img_div_4"><img class="Carouimg" src="https://daanvr.github.io/de_straat_op_maat/img/Str1/imgs/4.JPG">Fietsen staan op veel plekken geplaatst waar het niet hoort<div>');
+$('.carousel').append('<div class="Carouimgdiv" id="img_div_5"><img class="Carouimg" src="https://daanvr.github.io/de_straat_op_maat/img/Str1/imgs/5.JPG">Hangende verlichting<div>');
+
+$('.VoorenNa').append('<div class="Carouimgdiv" id="img_div_6"><img class="Carouimg" src="https://daanvr.github.io/de_straat_op_maat/img/Str1/imgs/OUD.JPG">Fietsen staan op veel plekken geplaatst waar het niet hoort<div>');
+$('.VoorenNa').append('<div class="Carouimgdiv" id="img_div_7"><img class="Carouimg" src="https://daanvr.github.io/de_straat_op_maat/img/Str1/imgs/2.JPG">Hangende verlichting<div>');
 
 
 
 $(document).ready(function () {
     $('.carousel').slick({
-        slidesToShow: 3,
+        slidesToShow: 2,
         infinite: true,
         dots: true,
         variableWidth: true,
-        slidesToScroll: 1
-        // centerMode: true,
+        arrows: false,
+        centerMode: true,
+        // centerPadding: '60px',
+        speed: 100,
 
-        //   setting-name: setting-value
+        // fade: true,
+        // slidesToScroll: 1
+    });
+
+    $('.VoorenNa').slick({
+        slidesToShow: 1,
+        infinite: true,
+        dots: false,
+        variableWidth: false,
+        arrows: false,
+        centerMode: true,
+        // centerPadding: '60px',
+        speed: 300,
+        fade: true,
+        // slidesToScroll: 1
     });
 });
